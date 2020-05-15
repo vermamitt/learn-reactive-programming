84910dc1a2ccab60ec4b44e3e5c5049b238020fb
KillShotGhost <coolghostuni@gmail.com>
2020-05-15 14:43:17 +0530

    test cases with assertions

--- src/test/java/com/reactive/learn/demo/test/FluxTest.java
+++ src/test/java/com/reactive/learn/demo/test/FluxTest.java
@@ -2,6 +2,7 @@ package com.reactive.learn.demo.test;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 public class FluxTest {
 
@@ -68,5 +69,59 @@ public class FluxTest {
         items
                 .subscribe(System.out::println, e -> System.err.println("Exception is: " + e.getMessage()),
                         () -> System.out.println("Completed"));
+    }
+
+    @Test
+    public void should_assert_flux_without_error(){
+
+        Flux<String> items = Flux.just("Item 1", "Item 2", "Item 3")
+                .log();
+
+        StepVerifier.create(items)
+                .expectNext("Item 1")
+                .expectNext("Item 2")
+                .expectNext("Item 3")
+                .verifyComplete();
+    }
+
+    @Test
+    public void should_assert_flux_with_error(){
+
+        Flux<String> items = Flux.just("Item 1", "Item 2", "Item 3")
+                .concatWith(Flux.error(new RuntimeException("Custom Runtime Error")))
+                .log();
+
+        StepVerifier.create(items)
+                .expectNext("Item 1")
+                .expectNext("Item 2")
+                .expectNext("Item 3")
+                .expectError(RuntimeException.class)
+                .verify();
+    }
+
+    @Test
+    public void should_assert_flux_with_error_message(){
+
+        Flux<String> items = Flux.just("Item 1", "Item 2", "Item 3")
+                .concatWith(Flux.error(new RuntimeException("Custom Runtime Error")))
+                .log();
+
+        StepVerifier.create(items)
+                .expectNext("Item 1", "Item 2", "Item 3")
+                .expectErrorMessage("Custom Runtime Error")
+                .verify();
+    }
+
+    @Test
+    public void should_assert_flux_with_count(){
+
+        Flux<String> items = Flux.just("Item 1", "Item 2", "Item 3")
+                .concatWith(Flux.error(new RuntimeException("Custom Runtime Error")))
+                .log();
+
+        StepVerifier.create(items)
+                .expectNextCount(3)
+                .expectErrorMessage("Custom Runtime Error")
+                .verify();
     }
 }
