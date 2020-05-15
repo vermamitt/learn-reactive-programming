3332f5d36d85097d95f76df4d7ba7e57436df790
KillShotGhost <coolghostuni@gmail.com>
2020-05-15 11:33:18 +0530

    initial setup

---
+++ src/main/java/com/reactive/learn/LearnReactiveProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnReactiveProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnReactiveProgrammingApplication.class, args);
+	}
+
+}
